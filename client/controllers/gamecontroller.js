58bbc3f6443b4a2195422b8e5913e7295584fe6d
Per Jansson <per.r.jansson@gmail.com>
2014-03-07 10:15:39 +0100

    Added a cell object to each cell.

--- client/controllers/gamecontroller.js
+++ client/controllers/gamecontroller.js
@@ -1,9 +1,15 @@
 var myApp = angular.module('BattleShipApp', []);
 
 myApp.controller('GameController', ['$scope', function ($scope) {
+
+    var cell = { hit: '0'  };
+
     var row = new Array(10);
     for (var i = 0; i < 10; i++) {
         row[i] = new Array(10);
+        for (var j = 0; j < 10; j++) {
+            row[i][j] = cell;
+        }
     }
     $scope.board = row;
 }]);
